48d00438553a95c0c7cb7e2e6dd4d0ec31515bfb
Henry Gomez <henrygomez@MacStudodeHenry.mre.corp>
2026-07-07 10:12:40 -0500

    fix: remove deprecated next.config props

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
   headers: async () => [
     {
       source: '/api/:path*',
